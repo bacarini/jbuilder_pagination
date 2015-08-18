cdb8c9d2001b7c318cfc074e9c51248cf6cb1716
Jose Galisteo <ceritium@gmail.com>
2015-08-19 00:44:47 +0200

    Methods and params for pagination are configurables

--- lib/jbuilder/pagination/pages.rb
+++ lib/jbuilder/pagination/pages.rb
@@ -1,29 +1,41 @@
 class Jbuilder
+
   def pages!(collection, options={})
     return unless collection
 
     pages do
       pages_from(collection).map do |k, v|
-        _set_value k, "#{options[:url]}?page=#{v}&per_page=#{collection.per_page}"
+        _set_value k, "#{options[:url]}?#{page_param}=#{v}&#{per_page_param}=#{collection.send(per_page_method)}"
       end
     end
   end
 
   private
 
+  delegate :current_page_method, :total_pages_method, :per_page_method, :page_param, :per_page_param, to: :configuration
+
+  def configuration
+    Pagination.configuration
+  end
+
+
   def pages_from(collection)
-    return {} if collection.total_pages == 1
+    total_pages = collection.send(total_pages_method)
+    current_page = collection.send(configuration.current_page_method)
+
+    return {} if total_pages == 1
 
     {}.tap do |pages|
-      unless collection.current_page == 1
+      unless current_page == 1
         pages[:first] = 1
-        pages[:prev]  = collection.current_page - 1
+        pages[:prev]  = current_page - 1
       end
 
-      unless collection.current_page == collection.total_pages
-        pages[:last] = collection.total_pages
-        pages[:next] = collection.current_page + 1
+      unless current_page == total_pages
+        pages[:last] = total_pages
+        pages[:next] = current_page + 1
       end
     end
   end
+
 end
