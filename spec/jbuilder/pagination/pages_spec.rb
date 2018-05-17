9d687605eda829d1926834a7f69b9d607a780dce
scytherswings <scytherswings@gmail.com>
2018-05-17 14:27:52 -0400

    fixed issue where nil additional parameters broke everything

--- spec/jbuilder/pagination/pages_spec.rb
+++ spec/jbuilder/pagination/pages_spec.rb
@@ -16,7 +16,15 @@ describe 'Jbuilder#pages!' do
     it { expect(build_json_for(collection, query_parameters: additional )).to eq(response_json) }
   end
 
-  context 'when there is no pagination for collection' do
+  context 'when there are nil query_parameters it still works' do
+    let(:collection) { OpenStruct.new(current_page: 2, total_pages: 3, size: 1) }
+    let(:response_json) {File.read("spec/fixtures/links.json").chomp }
+    let(:additional) { { test: 'test' } }
+
+    it { expect(build_bad_json_for(collection)).to eq(response_json) }
+  end
+
+  context 'when there is no pagination for collection it doesnt blow up' do
     let(:collection) { OpenStruct.new(current_page: 1, total_pages: 1, size: 1) }
     let(:response_json) { {links: {self: "https://api.example.com/v1/servers?page%5Bnumber%5D=1&page%5Bsize%5D=1" } }.to_json }
 
@@ -39,6 +47,16 @@ describe 'Jbuilder#pages!' do
         json.pages! collection,
           url: "https://api.example.com/v1/servers",
           query_parameters: options.fetch(:query_parameters, {})
+      end
+    end
+  end
+
+  def build_bad_json_for(collection)
+    Jbuilder.encode do |json|
+      json.links do
+        json.pages! collection,
+                    url: "https://api.example.com/v1/servers",
+                    query_parameters: nil
       end
     end
   end
